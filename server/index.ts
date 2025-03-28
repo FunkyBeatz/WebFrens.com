236a7a69f1b022b886c5b239c83638727e711c48
FunkyBeatz <funkybeatzeth@gmail.com>
2025-03-28 23:38:06 +0100

    Update: Configure static file serving for production

--- server/index.ts
+++ server/index.ts
@@ -83,9 +83,9 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 
 // Serve static files in production
 if (config.server.nodeEnv === 'production') {
-  app.use(express.static(join(__dirname, '../public')));
+  app.use(express.static(join(__dirname, '../dist')));
   app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '../public/index.html'));
+    res.sendFile(join(__dirname, '../dist/index.html'));
   });
 }
 
